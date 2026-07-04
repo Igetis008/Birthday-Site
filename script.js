d0b77af7f0534f8e4d00376a4d159b1ee562e36f
Aric <202674923+Igetis008@users.noreply.github.com>
2026-07-05 02:13:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -248,9 +248,6 @@ function onPlayerStateChange(event) {
 // automatic post-voice-greeting trigger, so the UI stays in sync either way
 function startMusicPlayback() {
     player.playVideo();
-   setTimeout(() => {
-    console.log(player.getPlayerState());
-}, 500);
     musicToggleBtn.classList.remove("muted");
     musicToggleBtn.querySelector(".music-icon").classList.add("icon-playing");
     isAudioPlaying = true;
